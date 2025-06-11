0fc3a97c164f0af4ea2e9b2d73691b5cdadb5941
Cory A. Jodon <137443282+cjodon01@users.noreply.github.com>
2025-06-10 20:25:57 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './contexts/AuthContext'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { Layout } from './components/Layout'
 import { Login } from './pages/Login'
-import { Dashboard } from './pages/Dashboard'
-import { Users } from './pages/Users'
-import { UserManagement } from './pages/UserManagement'
-import { SocialConnections } from './pages/SocialConnections'
-import { SocialPagesManagement } from './pages/SocialPagesManagement'
-import { Campaigns } from './pages/Campaigns'
-import { Brands } from './pages/Brands'
-import { AIConfig } from './pages/AIConfig'
-import { ContentLog } from './pages/ContentLog'
-import { Surveys } from './pages/Surveys'
-import { AccessDenied } from './pages/AccessDenied'
-import { SurveyManagement } from './pages/SurveyManagement'
-import { AnalyticsManagement } from './pages/AnalyticsManagement'
+import { MasterPage } from './pages/MasterPage'
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
         <Routes>
-          {/* Default route - login page */}
-          <Route path="/" element={<Login />} />
-          
-          {/* Public routes */}
           <Route path="/login" element={<Login />} />
-          
-          {/* Protected routes */}
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/users" element={
-            <ProtectedRoute>
-              <Layout>
-                <Users />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/user-management" element={
-            <ProtectedRoute>
-              <Layout>
-                <UserManagement />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/social-connections" element={
-            <ProtectedRoute>
-              <Layout>
-                <SocialConnections />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/social-pages" element={
-            <ProtectedRoute>
-              <Layout>
-                <SocialPagesManagement />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/campaigns" element={
-            <ProtectedRoute>
-              <Layout>
-                <Campaigns />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/brands" element={
-            <ProtectedRoute>
-              <Layout>
-                <Brands />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/ai-config" element={
-            <ProtectedRoute>
-              <Layout>
-                <AIConfig />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/content-log" element={
-            <ProtectedRoute>
-              <Layout>
-                <ContentLog />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/surveys" element={
-            <ProtectedRoute>
-              <Layout>
-                <Surveys />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/survey-management" element={
-            <ProtectedRoute>
-              <Layout>
-                <SurveyManagement />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/analytics" element={
-            <ProtectedRoute>
-              <Layout>
-                <AnalyticsManagement />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/denied" element={
-            <ProtectedRoute>
-              <Layout>
-                <AccessDenied/>
-              </Layout>
-            </ProtectedRoute>
-          } />
+          <Route path="/*" element={<MasterPage />} />
         </Routes>
         <Toaster position="top-right" />
       </div>

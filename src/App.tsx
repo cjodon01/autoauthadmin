d139479ccc40c14bdc3f75630cad47ac591f0d2d
Cory A. Jodon <137443282+cjodon01@users.noreply.github.com>
2025-06-10 20:14:58 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Brands } from './pages/Brands'
 import { AIConfig } from './pages/AIConfig'
 import { ContentLog } from './pages/ContentLog'
 import { Surveys } from './pages/Surveys'
+import { AccessDenied } from './pages/AcceddDenied'
 import { SurveyManagement } from './pages/SurveyManagement'
 import { AnalyticsManagement } from './pages/AnalyticsManagement'
 
